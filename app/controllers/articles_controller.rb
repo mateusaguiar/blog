fce91f5fb0ff990e0084cd9289be9d24d4fda22b
Mateus Aguiar <m_aguiar@Mateuss-MacBook-Pro.local>
2023-06-28 15:38:22 -0300

    feat: add show, update and delete articles routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,29 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(title: params[:title], body: params[:body])
+    @article = Article.create!(title: params[:title], body: params[:body])
 
     render json: @article, status: :created
   end
+
+  def show
+    @article = Article.find(params[:id])
+
+    render json: @article, status: :ok
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update!(title: params[:title], body: params[:body])
+
+    render json: @article, status: :ok
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy!
+
+    render json: @article, status: :ok
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/articles', to: 'articles#index'
   post '/articles', to: 'articles#create'
+  get '/articles/:id', to: 'articles#show'
+  put '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
 end
